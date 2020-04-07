46b9f679389a0a71951c1eb00f6440c904c7ec30
ajeetpratapsingh <ajeetpratapsingh@ajeets-mbp>
2020-04-07 08:44:16 +0530

    added regression test case

--- MyMavenProjectName/src/test/java/MyMavenGroupID/MyMavenProjectName/UniqueTest.java
+++ MyMavenProjectName/src/test/java/MyMavenGroupID/MyMavenProjectName/UniqueTest.java
@@ -9,5 +9,15 @@ public class UniqueTest {
 public void joker() {
 	System.out.println("Test print here ...");
 }
+
+@Test
+public void joker1() {
+	System.out.println("regression 2 ...");
+}
+
+@Test
+public void joker2() {
+	System.out.println("regression3 ...");
+}
 	
 }
